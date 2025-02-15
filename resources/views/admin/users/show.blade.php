68affa3a084364602c08740f1a654836c440c819
fajarriswandi-yumna <fajarriswandi.yumna@gmail.com>
2025-02-15 14:26:06 +0700

    style users done

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -3,34 +3,42 @@
 @section('title', 'Detail User Admin')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="h3 mb-4 text-gray-800">Detail User Admin</h1>
+    <div class="container">
+        <div class="mb-2 d-flex justify-content-between align-items-center">
+            <h1 class="titlePage">Detail Users</h1>
+            <div>breadcrumb</div>
+        </div>
 
         <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Informasi User Admin</h6>
-            </div>
-            <div class="card-body">
-                <div class="text-center mb-3">
-                    @if($user->profile_image_path)
-                        <img src="{{ asset('storage/' . $user->profile_image_path) }}" alt="Gambar Profil" class="rounded-circle" width="150">
-                    @else
-                        <img src="{{ asset('img/default_profile.png') }}" alt="Gambar Profil Default" class="rounded-circle" width="150">
-                    @endif
+            <div class="card-header cardHeaderPrimary">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="{{ route('admin.users.index') }}" class="btn btn-outline-light"><i
+                            class="fas fa-arrow-left"></i></a>
+                    <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-outline-light"><i
+                            class="fas fa-edit"></i></a>
+
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Nama Lengkap</label>
-                    <p>{{ $user->name }}</p>
+                <div class="headerProfile">
+                    @if ($user->profile_image_path)
+                        <img src="{{ asset('storage/' . $user->profile_image_path) }}" alt="Gambar Profil"
+                            class="rounded-circle" width="150">
+                    @else
+                        <img src="{{ asset('images/emptyImage.png') }}" alt="Gambar Profil Default" class="rounded-circle"
+                            width="150">
+                    @endif
+                </div>
+            </div>
+            <div class="card-body pt-5">
+
+                <div class="mb-3 pt-2">
+                    <h2>{{ $user->name }}</h2>
                 </div>
 
                 <div class="mb-3">
                     <label class="form-label">Email</label>
                     <p>{{ $user->email }}</p>
                 </div>
-
-                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-warning">Edit</a>
-                <a href="{{ route('admin.users.index') }}" class="btn btn-secondary">Kembali ke Daftar User Admin</a>
             </div>
         </div>
     </div>
